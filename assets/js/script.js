50537dde3dc9289be9114c6ac6e153ccfe4a3167
Anjali Roland <anjaligroland@gmail.com>
2023-03-28 15:38:10 -0700

    broke down project into to do's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 // Assignment code here
 
+// TODO: create variables for character options; lowercase, uppercase, numeric, & special characters
+
+// TODO: create a function that asks the following:
+              //prompt for length of password with 8 character min and 128 character max
+                    // if accepted number following prompts will be asked:
+                          //"Do you want your new password to include lowercase letters?"
+                          //"Do you want your new password to include uppercase letters?"
+                          //"Do you want your new password to include numbers?"
+                          //"Do you want your new password to include special characters? "
+                    // if not user is told to try again
+
+// TODO: create IF statement checking if atleast one character has been selected
+              //if yes then password will be generated and displayed
+              //if no then asked to repeat prompts
+
+// TODO: create function to write a password meeting selected criteria
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
