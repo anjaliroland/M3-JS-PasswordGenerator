a98b2d4f8db5fbfc5018de9ec18a14775c42d16b
Anjali Roland <anjaligroland@gmail.com>
2023-03-28 16:32:50 -0700

    updated a to do comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
-//variables for character options; lowercase, uppercase, numeric, & special characters
+// TODO: create variables for character options; lowercase, uppercase, numeric, & special characters
 var lowercaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numberChar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
@@ -8,18 +8,18 @@ var specialChar = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '
 
 
 // TODO: create a function that asks the following:
-              //prompt for length of password with 8 character min and 128 character max
+              //"How many characters would you like your password to have? (Must between 8 - 128 characters)"
                     // if accepted number following prompts will be asked:
                           //"Do you want your new password to include lowercase letters?"
                           //"Do you want your new password to include uppercase letters?"
                           //"Do you want your new password to include numbers?"
-                          //"Do you want your new password to include special characters? "
-                    // if not user is told to try again
+                          //"Do you want your new password to include special characters?"
+                    // if not user is told "Invalid input, character length must be between 8 - 128 characters. Please try again."
 
 // TODO: create IF statement checking if atleast one character has been selected
               //if yes then password will be generated and displayed
               //if no then asked to repeat prompts
-
+              
 // TODO: create function to write a password meeting selected criteria
 
 
