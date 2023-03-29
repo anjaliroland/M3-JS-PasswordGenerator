c537fb85362a0825cf6ff6ec953ffa8880315a5b
Anjali Roland <anjaligroland@gmail.com>
2023-03-29 15:24:29 -0700

    wrote code for prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,19 @@
   //TODO: numeric✔️
   //TODO: special✔️
 
-// TODO: create a variable for selected criteria
+// TODO: create a variable for selected criteria✅
 
-//TODO: create a function that asks the following:
-  //TODO:"How many characters would you like your new password to have? (Must between 8 - 128 characters)"
-    //TODO: if accepted number following prompts will be asked:
-      //TODO:"Do you want your new password to include lowercase letters?"
-      //TODO:"Do you want your new password to include uppercase letters?"
-      //TODO:"Do you want your new password to include numbers?"
-      //TODO:"Do you want your new password to include special characters?"
-    //TODO:if not user is told "Invalid input, character length must be between 8 - 128 characters. Please try again."
-
-//TODO:create IF statement checking if atleast one character has been selected
-  //TODO:if yes then password will be generated and displayed
-  //TODO:if no then asked to repeat prompts
+// TODO: make generatePassword()✅
+  //TODO: create an prompt asking: "How many characters would you like your new password to have? (Must between 8 - 128)"✔️
+      //TODO:if not user is told "Invalid input, password length must be between 8 - 128 characters. Please try again."✔️
+      //TODO: if accepted number following confirms will be asked:
+        //TODO:"Do you want your new password to include lowercase letters?"✔️
+        //TODO:"Do you want your new password to include uppercase letters?"✔️
+        //TODO:"Do you want your new password to include numbers?"✔️
+        //TODO:"Do you want your new password to include special characters?"✔️   
+  //TODO:create IF statement checking if atleast one character has been selected
+    //TODO:if yes then password will be generated and displayed
+    //TODO:if no then asked to repeat prompts
 
 // TODO: create function to write a password meeting selected criteria
 
@@ -27,24 +26,34 @@ var uppercaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 var numberChar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var specialChar = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', '<', '=>', '?', '@', '[', "]", '^', "_", "{", "|", "}", "~"];
 
+var passwordArr = [];
 
+function generatePassword() {
+  charLength = parseInt(prompt("How many characters would you like your new password to have? (Must between 8 - 128)"));
+  if (isNaN(charLength) || charLength < 8 || charLength > 128) {
+    window.alert("Invalid input, password length must be between 8 - 128 characters. Please try again.");
+  } else {
+    if (confirm("Do you want your new password to include lowercase letters?")) {
+      passwordArr = passwordArr.concat(lowercaseChar);
+    }
+    if (confirm("Do you want your new password to include uppercase letters?")) {
+      passwordArr = passwordArr.concat(uppercaseChar);
+    }
+    if (confirm("Do you want your new password to include numbers?")) {
+      passwordArr = passwordArr.concat(numberChar);
+    }
+    if (confirm("Do you want your new password to include special characters?")) {
+      passwordArr = passwordArr.concat(specialChar);
+    } 
+  }
 
-function askPrompts() {
-  charLength = parseInt(prompt("How many characters would you like your new password to have? (Must between 8 - 128 characters)"));
-  var validLength = (charLength >= 8 && charLength <= 128);
-  if (charLength === validLength) {
-    confirm("Do you want your new password to include lowercase letters?");
+  for (let i = 0; i < validLength.length; i++) {
+    var randomChar = passwordArr[Math.floor(Math.random() *charLength.length)];
+    
   }
 
 };
-
-function validInput() {
-
-};
               
-function generatePassword() {
-
-};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -60,3 +69,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
