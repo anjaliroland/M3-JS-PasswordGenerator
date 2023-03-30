c722b1bc75f291a5192c9d678328f43caaf3012e
Anjali Roland <anjaligroland@gmail.com>
2023-03-29 17:59:08 -0700

    deleted To Do's and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,26 @@
 // Assignment code here
-// TODO: create variables for character options✅
-  //TODO: lowercase✔️
-  //TODO: uppercase✔️
-  //TODO: numeric✔️
-  //TODO: special✔️
 
-// TODO: create a variable for selected criteria✅
-
-// TODO: make generatePassword function that creates password based on selected criteria✅
-  //TODO: create an prompt asking: "How many characters would you like your new password to have? (Must between 8 - 128)"✔️
-      //TODO:if not user is told "Invalid input, password length must be between 8 - 128 characters. Please try again."✔️
-      //TODO: if accepted number following confirms will be asked:
-        //TODO:"Do you want your new password to include lowercase letters?"✔️
-        //TODO:"Do you want your new password to include uppercase letters?"✔️
-        //TODO:"Do you want your new password to include numbers?"✔️
-        //TODO:"Do you want your new password to include special characters?"✔️   
-
-  //TODO:create IF statement checking if atleast one character has been selected✅
-    //TODO:if yes then password will be generated and displayed✔️
-    //TODO:if no then asked to repeat prompts✔️
-
+// character variables
 var lowercaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numberChar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var specialChar = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', '<', '=>', '?', '@', '[', "]", '^', "_", "{", "|", "}", "~"];
 
+// variable to hold selected criteria
 var passwordArr = [];
 
+
 function generatePassword() {
+// prompt determining character length
   charLength = parseInt(prompt("How many characters would you like your new password to have? (Must between 8 - 128)"));
 
+// validates the number entered is in accepted range
   if (isNaN(charLength) || charLength < 8 || charLength > 128) {
     window.alert("Invalid input, password length must be between 8 - 128 characters. Please try again.");
     generatePassword();
-  } else {
+
+// popups confirming criteria to be met
+  } else {  
     if (confirm("Do you want your new password to include lowercase letters?")) {
       passwordArr = passwordArr.concat(lowercaseChar);
     }
@@ -48,15 +35,19 @@ function generatePassword() {
     } 
   } ;
 
+// validates at least one character type has been chosen
   if (passwordArr.length === 0) {
     window.alert("Must select at least one character type.");
     generatePassword();
+
+// randomly chooses characters from the array made of selected criteria
   } else {
     var password = "";
     for (let i = 0; i < charLength; i++) {
       password += passwordArr[Math.floor(Math.random() *passwordArr.length)];
     };
 
+// ties function into code below to display generated password
   return password;
   };
 };
